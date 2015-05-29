a7576f6f9d6bb38ad3529b601ad24877dbb48fc1
de-lujo <hip-hop_de-lujo@hotmail.com>
2015-05-29 09:53:52 -0300

    se agrega la paginación de las consultas

--- application/controllers/consulta1.php
+++ application/controllers/consulta1.php
@@ -8,7 +8,18 @@ class consulta1 extends CI_Controller {
     public function index() {
 
     //    $this->load->database();
-       $query = $this->db->query("SELECT concat_ws(' ',first_name,last_name) as nombre_apellido, birth_date, gender  from employees where gender='M' AND birth_date<'1954-01-01'");
+
+
+        $this->load->library('pagination');
+        $config['base_url'] = 'http://192.168.1.33:8081/tarea2/index.php/consulta1/';
+        $config['per_page'] = 100;
+        $config['total_rows'] =500;
+        $getD= $this->uri->segment(4);
+        if( $getD>0 ) $config['per_page'] = $getD;
+        $this->pagination->initialize($config); 
+        $this->db->cache_on();
+
+       $query = $this->db->query("SELECT concat_ws(' ',first_name,last_name) as nombre_apellido, birth_date, gender  from employees where gender='M' AND birth_date<'1954-01-01' LIMIT 0".",".$config['per_page']);
         $d = "<table>
                 <tr>
                   <th scope='col'><strong>nombre_apellido</strong></th>
@@ -26,10 +37,21 @@ class consulta1 extends CI_Controller {
         $this->load->view('consulta1',$datosAEnviar);
     }
 
+
     public function hola() {
         //$this->load->database();
+
+        $this->load->library('pagination');
+        $config['base_url'] = 'http://192.168.1.33:8081/tarea2/index.php/consulta1/hola/';
+        $config['per_page'] = 20;
+        $config['total_rows'] =500;
+        $getD= $this->uri->segment(3);
+
+        if( $getD>0 ) $config['per_page'] = $getD;
+        $this->pagination->initialize($config); 
         $this->db->cache_on();
-        $query = $this->db->query('SELECT dept_name FROM departments');
+
+        $query = $this->db->query("SELECT dept_name FROM departments LIMIT 0".",".$config['per_page']);
 
         $d ="<table>
                 <tr>
@@ -47,8 +69,20 @@ class consulta1 extends CI_Controller {
 
     public function chao() {
         //$this->load->database();
+
+        $this->load->library('pagination');
+        $config['base_url'] = 'http://192.168.1.33:8081/tarea2/index.php/consulta1/chao/';
+        $config['per_page'] = 20;
+        $config['total_rows'] =500;
+        $getD= $this->uri->segment(3);
+
+        if( $getD>0 ) $config['per_page'] = $getD;
+        $this->pagination->initialize($config); 
         $this->db->cache_on();
-        $query = $this->db->query("SELECT e.emp_no as emp_no, e.first_name as first_name, s.salary as salary, d.dept_no as dept_no, dep.dept_name as dept_name  FROM employees e, salaries s, dept_emp  d, departments dep WHERE e.emp_no=s.emp_no AND s.emp_no < 23000 AND e.emp_no=d.emp_no AND d.dept_no=dep.dept_no GROUP BY e.emp_no");
+        $query = $this->db->query("SELECT e.emp_no as emp_no, e.first_name as first_name, s.salary as salary, d.dept_no as dept_no, dep.dept_name as dept_name  FROM employees e, salaries s, dept_emp  d, departments dep WHERE e.emp_no=s.emp_no AND e.emp_no < 20000 AND s.salary >109000 AND e.emp_no=d.emp_no AND d.dept_no=dep.dept_no GROUP BY e.emp_no LIMIT 0".",".$config['per_page']);
+        
+        
+
 //as emp_no, e.first_name as first_name, s.salary as salary, d.dept_no as dept_no, dep.dept_name as dept_name 
         $d = "<table>
                 <tr>
--- application/views/consulta1.php
+++ application/views/consulta1.php
@@ -5,6 +5,6 @@
 <body>
 <h1>Consulta: </h1>
 
-<p> <?php echo $hola;?> </p>
+<p> <?php echo $hola;  echo $this->pagination->create_links();?> </p>
 </body>
 </html>
